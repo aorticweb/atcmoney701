ce21c3c8f2700f0c1d08e8b434b90cb96478aceb
aorticweb <aorticweb@aorticweb.com>
2023-02-28 22:09:09 -0500

    oops forgot the tests

---
+++ providers/smoke_tests/test_vantage_client.py
@@ -0,0 +1,26 @@
+import pytest
+from providers import ClientFactory, ClientType
+from providers.common.currency import Currency
+from providers.common.quote import Quote
+from providers.exception import ProviderAPIError
+client_generator = ClientFactory[ClientType.VANTAGE]
+
+
+def test_get_quote():
+    """
+    Test getting a quote from vantage
+    """
+    client = client_generator()
+    quote = client.get_quote("GOOGL")
+    assert isinstance(quote, Quote)
+    assert quote.currency == Currency.USD
+    assert isinstance(quote.price, float)
+
+
+def test_get_quote_fake_symbol_raises_error():
+    """
+    Test getting a quote from vantage
+    """
+    client = client_generator()
+    with pytest.raises(ProviderAPIError):
+        client.get_quote("BAD_SYMBOL_INVALID")
